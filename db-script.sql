0a2d341bd74c3e7c2153b2957630edd921741525
Sachith Rangana1 <sachith2546@gmail.com>
2024-08-05 21:48:57 +0530

    add sachith

--- db-script.sql
+++ db-script.sql
@@ -3,3 +3,47 @@ CREATE TABLE sachith(
     name VARCHAR(100) NOT NULL ,
     credits INT NOT NULL
 );
+CREATE TABLE program
+(
+    program_id VARCHAR(10) PRIMARY KEY,
+    name       VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE module
+(
+    id      VARCHAR(10) PRIMARY KEY,
+    name    VARCHAR(100) NOT NULL,
+    credits INT          NOT NULL CREATE TABLE student
+(
+    id   VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE module
+(
+    id      VARCHAR(10) PRIMARY KEY,
+    name    VARCHAR(100) NOT NULL,
+    credits INT          NOT NULL
+);
+
+CREATE TABLE exam
+(
+    exam_code     VARCHAR(10) PRIMARY KEY,
+    passing_score INT         NOT NULL,
+    module_id     VARCHAR(10) NOT NULL,
+    CONSTRAINT fk_module_id FOREIGN KEY (module_id) REFERENCES module (id)
+);
+CREATE TABLE program_module
+(
+    program_id VARCHAR(10) NOT NULL,
+    module_id  VARCHAR(10) NOT NULL,
+    CONSTRAINT pk_program_module PRIMARY KEY (program_id, module_id)
+);
+
+CREATE TABLE batch
+(
+    number      VARCHAR(100) PRIMARY KEY,
+    programm_id VARCHAR(10) NOT NULL
+);
+
+
