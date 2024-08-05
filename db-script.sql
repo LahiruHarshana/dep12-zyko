3f655f2317e27dadfcc0728dea9c0192dc257c29
sanduni <sanduniakn@gmail.com>
2024-08-05 21:05:41 +0530

    created student_exam table

--- db-script.sql
+++ db-script.sql
@@ -9,3 +9,12 @@ CREATE TABLE exam(
     module_id VARCHAR(10) NOT NULL ,
     CONSTRAINT fk_module_id FOREIGN KEY (module_id) REFERENCES module(id)
 );
+
+CREATE TABLE student_exam(
+    student_id VARCHAR(10) NOT NULL,
+    exam_code VARCHAR(10) NOT NULL ,
+    marks INT NOT NULL,
+    CONSTRAINT pk_take PRIMARY KEY (student_id, exam_code),
+    CONSTRAINT fk_student_id FOREIGN KEY (student_id) REFERENCES student(id)
+    CONSTRAINT fk_exam_code FOREIGN KEY (exam_code) REFERENCES exam(code)
+);
